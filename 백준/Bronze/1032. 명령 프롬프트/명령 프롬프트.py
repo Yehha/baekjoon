5f02a081fec51eccb1e42bbf59185d25641bffe3
Yehha <90622888+Yehha@users.noreply.github.com>
2025-04-03 00:59:58 +0900

    Update 명령 프롬프트.py

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -1,16 +1,21 @@
 import sys
 
-# 문자열 N줄 입력받아 리스트에 저장
-N =  int(sys.stdin.readline()) # 입력받을 파일 이름 갯수
-data = [sys.stdin.readline().strip() for i in range(N)] # 입력받은 파일 이름 리스트
-result = [] #결과 값 담을 리스트
+# N개 문자열(파일이름) 입력받기
+N =  int(sys.stdin.readline()) # 입력받을 파일 갯수
+data = [sys.stdin.readline().strip() for i in range(N)] # 파일 이름 리스트
 
+result = [] #결과 문자열 저장할 리스트
+
+# 첫 번째 문자열을 기준으로 동일한 위치의 문자를 비교
 for i in range(len(data[0])):
-    word = data[0][i] # 비교할 i번쨰 문자 
-    for j in range(1,len(data)): # 다른 파일 이름들과 i번쨰 문자를 비교
-        if word != data[j][i]:
+    word = data[0][i] # 첫 번째 파일의 i번째 문자
+
+    for j in range(1,len(data)): # 다른 파일들과 i번쨰 문자 비교
+        if word != data[j][i]: # 하나라도 다르면 '?'로 변경
             word = '?'
             break
-    result.append(word)
+    result.append(word) # 결과 리스트에 추가
 
+
+# 리스트를 문자열로 변환 후 출력
 print(''.join(result))
